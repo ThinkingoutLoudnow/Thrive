c1f20e938995d2f80a142e29dab638c80ffcf202
Armand Paquette <apaquette009@gmail.com>
2018-02-15 19:29:38 -0500

    again with navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :products
   resources :object_thrives
 
-get '/products', to: 'products#index'
-
   root 'simple_pages#index'
 
   get 'simple_pages/index'
