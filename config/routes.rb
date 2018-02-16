088ac99580ac2ccc7a6bbd773e0ca82eb26718c1
Armand Paquette <apaquette009@gmail.com>
2018-02-15 19:00:18 -0500

    linked product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
   resources :object_thrives
+  
+get '/products', to: 'products#index'
   root 'simple_pages#index'
 
   get 'simple_pages/index'
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
