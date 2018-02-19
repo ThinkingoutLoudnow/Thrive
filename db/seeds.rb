32c56c7f7f1a23b1c67aba2dde1f0896ccfc49c3
Armand Paquette <apaquette009@gmail.com>
2018-02-19 18:50:15 -0500

    added product using seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Thrive A", description: "$34.99", image_url: "https://img.thrivemarket.com/store/566x/6/7/671635701763-1_1_1.jpg")
+Product.create(name: "Thrive B", description: "$34.99", image_url: "https://img.thrivemarket.com/store/full/6/7/671635701053-1.jpg")
+Product.create(name: "Thrive C", description: "$34.99", image_url: "https://img.thrivemarket.com/store/full/6/7/671635701213-1.jpg")
+Product.create(name: "Thrive D", description: "$34.99", image_url: "https://img.thrivemarket.com/store/full/6/7/671635701053-1.jpg")
+Product.create(name: "Thrive E", description: "$34.99", image_url: "https://img.thrivemarket.com/store/566x/6/7/671635701763-1_1_1.jpg")
+Product.create(name: "Thrive F", description: "$34.99", image_url: "https://img.thrivemarket.com/store/full/6/7/671635701053-1.jpg")
