123a9f15f96d212c3a3e2450f1928a917ba031f4
Armand Paquette <apaquette009@gmail.com>
2018-02-19 19:55:24 -0500

    Trying to start over again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213233001) do
+ActiveRecord::Schema.define(version: 20180219235208) do
 
   create_table "object_thrives", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180213233001) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
 end
