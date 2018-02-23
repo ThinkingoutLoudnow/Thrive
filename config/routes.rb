29953450a77d3142bfeda5716d7a881789d39a03
Armand Paquette <apaquette009@gmail.com>
2018-02-23 12:11:29 -0500

    created index.html/show.html into orders folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :orders, only: [index, :show. :create. :destroy]
+
+
+
+  get 'orders/index'
 
   get 'simple_pages/show'
 
@@ -18,7 +21,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-
+ resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
